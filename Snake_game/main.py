f842b5e9c1c84356eff30cfa0f732eeb83ba61e5
Dreghici Ionut <dreionut98@gmail.com>
2024-07-17 08:36:31 +0100

    Complete snake game

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -22,21 +22,28 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
 game_is_on = True
-wall = [300, -300]
+wall = [280, -280]
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.clear()
         scoreboard.increase_score()
+
     # Detect collision with wall
     if snake.head.xcor() in wall or snake.head.ycor() in wall:
         scoreboard.game_over()
         break
+
+    # Detect collision with tail; If head collides with any segment in the tail
+    for segment in snake.segments[1:]: # Slicing
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
--- Snake_game/snake.py
+++ Snake_game/snake.py
@@ -12,19 +12,26 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.penup()
-            new_segment.color("white")
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.penup()
+        new_segment.color("white")
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def up(self):
         if self.head.heading() != DOWN:
