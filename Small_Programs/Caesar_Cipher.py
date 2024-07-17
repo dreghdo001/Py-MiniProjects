660e97d8dfe8d3c33e61ce0e6a6ca5e5b1570382
Dreghici Ionut <dreionut98@gmail.com>
2024-07-17 07:09:20 +0100

    First commit: All projects

--- Small_Programs/Caesar_Cipher.py
+++ Small_Programs/Caesar_Cipher.py
@@ -1,7 +1,10 @@
 import numbers
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
@@ -15,18 +18,17 @@ def caesar(start_text, shift_ammount, cipher_direction):
     for char in start_text:
         if char not in alphabet:
             end_text += char
-        else: 
+        else:
             position = alphabet.index(char)
             new_position = alphabet.index(char) + shift_ammount
             end_text += alphabet[new_position]
     print(f"\nThe {cipher_direction} text is : {end_text}")
 
+
 while direction == "encode" or direction == "decode":
-    
     text = input("Type your message:\n").lower()
     shift = int(input("Type th shift number:\n"))
-    
-    caesar(start_text=text, shift_ammount=shift, cipher_direction=direction)
-    
-    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
 
+    caesar(start_text=text, shift_ammount=shift, cipher_direction=direction)
+
+    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
