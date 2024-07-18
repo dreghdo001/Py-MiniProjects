1ae3b9c3cc2e6c4c6e6f0763837835f2664c48f6
Dreghici Ionut <dreionut98@gmail.com>
2024-07-18 15:46:48 +0100

    Pong Game - Add features - Complete

--- Pong_Game/main.py
+++ Pong_Game/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from players import Player
 from ball import Ball
+from score import Scoreboard
 import random
 
 DIRECTIONS = [45, 135,225,315]
@@ -13,8 +14,10 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
-r_paddle = Player((350,0))
-l_paddle = Player((-350,0))
+r_paddle = Player((350, 0))
+l_paddle = Player((-350, 0))
+r_score = Scoreboard((-70, 270))
+l_score = Scoreboard((70, 270))
 ball = Ball()
 
 screen.listen()
@@ -27,17 +30,27 @@ is_game_on = True
 
 while is_game_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
 
+    # Bounce the ball
     if ball.ycor() > 280 or ball.ycor() < -280:
         # needs to bounce
         ball.bounce_y()
-
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # Detect left paddle miss
+    if ball.xcor() > 380:
+        time.sleep(1)
+        r_score.increase_score()
+        ball.restart()
 
+    # Detect right paddle miss
+    if ball.xcor() < -380:
+        time.sleep(1)
+        l_score.increase_score()
+        ball.restart()
 
 
 
